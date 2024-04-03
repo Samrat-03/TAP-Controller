a7dcf3956e6e74e200589277b3dce06be6fd9a35
Samrat Chhabra <113725445+Samrat-03@users.noreply.github.com>
2024-04-03 17:45:50 +0530

    Create and_gate.v

---
+++ RTL/and_gate.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.03.2024 14:54:31
+// Design Name: 
+// Module Name: and_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_gate(
+    input a,b,
+    output c
+    );
+    
+    assign c = a & b;
+endmodule
